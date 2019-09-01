1cb2b43354f55571fc1872cb871d18b504abe08a
Brian Henderson <brianjhenderson@gmail.com>
2019-08-31 20:58:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Environment Setup
+
+Install common tools and configure ZSH, TMUX, VIM
+
+## Quick Start
+
+### Set zsh as your default shell
+
+To change zsh to your default shell run
+```bash
+chsh -s $(which zsh)
+```
+
+### Run setup
+
+```bash
+./setup.zsh
+```
---
+++ setup.zsh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )"
+
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+export HOMEBREW_CASK_OPTS=--require-sha
+
+echo "Installing homebrew ..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "... done"
+
+echo "Linking config files ..."
+mkdir -p ${ZDOTDIR:-$HOME}/.config
+for dotFile in zshrc config/starship.toml tmux.conf.local vimrc Brewfile
+do
+    if [ -L ${ZDOTDIR:-$HOME}/.$dotFile ]; then
+        rm ${ZDOTDIR:-$HOME}/.$dotFile
+    fi
+    ln -s $SCRIPT_DIR/configs/dotfiles/$dotFile ${ZDOTDIR:-$HOME}/.$dotFile
+done
+echo "... done"
+
+echo "Installing tools ..."
+brew bundle install --global
+$(brew --prefix)/opt/fzf/install --all
+chmod -R go-w $(brew --prefix)/share
+echo "... done"
+
+echo "Installing rust ..."
+rustup-init -y
+echo "... done"
+
+echo "Configuring tmux ..."
+git clone --recursive https://github.com/gpakosz/.tmux.git "${ZDOTDIR:-$HOME}/.tmux"
+if [ -L ${ZDOTDIR:-$HOME}/.tmux.conf ]; then
+    rm ${ZDOTDIR:-$HOME}/.tmux.conf
+fi
+ln -s "${ZDOTDIR:-$HOME}/.tmux/.tmux.conf" "${ZDOTDIR:-$HOME}/.tmux.conf"
+echo "... done"
+
+echo "Configuring vim ..."
+source  ${ZDOTDIR:-$HOME}/.zshrc
+vi +PlugInstall +GoInstallBinaries +qall
+echo "... done"
