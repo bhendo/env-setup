b48f206824f70d3920ed1891a21b64ecdd40e81f
Brian Henderson <brianjhenderson@gmail.com>
2019-10-22 10:49:03 -0400

    init tmux setup

---
+++ install_local_tmux.zsh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+
+
+ set -e
+
+ TMUX_VERSION=2.9a
+ LIBEVENT_VERSION=2.1.11-stable
+ NCURSES_VERSION=6.1
+
+ INSTALL_DIR=${ZDOTDIR:-$HOME}/.local
+ TEMP_DIR=${ZDOTDIR:-$HOME}/tmp
+
+ mkdir -p $INSTALL_DIR $TEMP_DIR
+ cd $TEMP_DIR
+
+ wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+ wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}/libevent-${LIBEVENT_VERSION}.tar.gz
+ wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
+
+ tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
+ cd libevent-${LIBEVENT_VERSION}
+ ./configure --prefix=$INSTALL_DIR --disable-shared
+ make
+ make install
+ cd ..
+
+if [[ $(fs --version) =~ "afs" ]] && fs whereis "$INSTALL_DIR" ; then
+    NCURSES_OPTION=" --enable-symlinks"
+else
+    NCURSES_OPTION=""
+fi
+
+tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
+cd ncurses-${NCURSES_VERSION}
+./configure --prefix=$INSTALL_DIR $NCURSES_OPTION
+make
+make install
+cd ..
+
+tar xvzf tmux-${TMUX_VERSION}.tar.gz
+cd tmux-${TMUX_VERSION}
+./configure CFLAGS="-I$INSTALL_DIR/include -I$INSTALL_DIR/include/ncurses" LDFLAGS="-L$INSTALL_DIR/lib -L$INSTALL_DIR/include/ncurses -L$INSTALL_DIR/include"
+CPPFLAGS="-I$INSTALL_DIR/include -I$INSTALL_DIR/include/ncurses" LDFLAGS="-static -L$INSTALL_DIR/include -L$INSTALL_DIR/include/ncurses -L$INSTALL_DIR/lib" make
+cp tmux $INSTALL_DIR/bin
+cd ..
+
+rm -rf $TEMP_DIR
+
+echo "$INSTALL_DIR/bin/tmux is now available. You can optionally add $INSTALL_DIR/bin to your PATH."
+
