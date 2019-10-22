81cd6fd82d75e9c5d9d724c3a460313be2da3283
Brian Henderson <brianjhenderson@gmail.com>
2019-10-22 10:54:08 -0400

    switch to curl

--- install_local_tmux.zsh
+++ install_local_tmux.zsh
@@ -1,28 +1,27 @@
 #!/bin/zsh
 
+set -e
 
- set -e
+TMUX_VERSION=2.9a
+LIBEVENT_VERSION=2.1.11-stable
+NCURSES_VERSION=6.1
 
- TMUX_VERSION=2.9a
- LIBEVENT_VERSION=2.1.11-stable
- NCURSES_VERSION=6.1
+INSTALL_DIR=${ZDOTDIR:-$HOME}/.local
+TEMP_DIR=${ZDOTDIR:-$HOME}/tmp
 
- INSTALL_DIR=${ZDOTDIR:-$HOME}/.local
- TEMP_DIR=${ZDOTDIR:-$HOME}/tmp
+mkdir -p $INSTALL_DIR $TEMP_DIR
+cd $TEMP_DIR
 
- mkdir -p $INSTALL_DIR $TEMP_DIR
- cd $TEMP_DIR
+curl -sL --proto-redir --all,https https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz > tmux-${TMUX_VERSION}.tar.gz
+curl -sL --proto-redir --all,https https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}/libevent-${LIBEVENT_VERSION}.tar.gz > libevent-${LIBEVENT_VERSION}.tar.gz
+curl ftp://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz > ncurses-${NCURSES_VERSION}.tar.gz
 
- wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
- wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}/libevent-${LIBEVENT_VERSION}.tar.gz
- wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
-
- tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
- cd libevent-${LIBEVENT_VERSION}
- ./configure --prefix=$INSTALL_DIR --disable-shared
- make
- make install
- cd ..
+tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
+cd libevent-${LIBEVENT_VERSION}
+./configure --prefix=$INSTALL_DIR --disable-shared
+make
+make install
+cd ..
 
 if [[ $(fs --version) =~ "afs" ]] && fs whereis "$INSTALL_DIR" ; then
     NCURSES_OPTION=" --enable-symlinks"
@@ -47,4 +46,3 @@ cd ..
 rm -rf $TEMP_DIR
 
 echo "$INSTALL_DIR/bin/tmux is now available. You can optionally add $INSTALL_DIR/bin to your PATH."
-
