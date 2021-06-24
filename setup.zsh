da87413999da5510af42ac0d38d9262e35a366d1
Brian Henderson <brianjhenderson@gmail.com>
2021-06-24 16:16:48 -0400

    Migrate to starship; reorg; add fonts

--- setup.zsh
+++ setup.zsh
@@ -1,22 +1,40 @@
 #!/bin/zsh
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+export HOMEBREW_CASK_OPTS=--require-sha
+
+echo "Install Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Install cli tools"
+brew install \
+fzf \
+reattach-to-user-namespace \
+starship \
+tmux \
+tokei \
+vim \
+zoxide \
+zsh-completions \
+zsh-history-substring-search
+
 echo "Configuring zsh"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/.zpreztorc > ${ZDOTDIR:-$HOME}/.zpreztorc
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/.zshrc > ${ZDOTDIR:-$HOME}/.zshrc
+mkdir -p "${ZDOTDIR:-$HOME}/.config"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/config_files/zshrc > ${ZDOTDIR:-$HOME}/.zshrc
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/config_files/starship.toml > ${ZDOTDIR:-$HOME}/.config/starship.toml
+$(brew --prefix)/opt/fzf/install --all
+chmod -R go-w $(brew --prefix)/share
 source  ${ZDOTDIR:-$HOME}/.zshrc
-zplug install
-zplug load
 echo "Done"
 
 echo "Configuring tmux"
 git clone --recursive https://github.com/gpakosz/.tmux.git "${ZDOTDIR:-$HOME}/.tmux"
 ln -s "${ZDOTDIR:-$HOME}/.tmux/.tmux.conf" "${ZDOTDIR:-$HOME}/.tmux.conf"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/.tmux.conf.local > ${ZDOTDIR:-$HOME}/.tmux.conf.local
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/config_files/tmux.conf.local > ${ZDOTDIR:-$HOME}/.tmux.conf.local
 echo "Done"
 
 echo "Configuring vim"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/.vimrc > ${ZDOTDIR:-$HOME}/.vimrc
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/bhendo/env-setup/master/config_files/vimrc > ${ZDOTDIR:-$HOME}/.vimrc
 vi +PlugInstall +GoInstallBinaries +qall
 echo "Done"
 
